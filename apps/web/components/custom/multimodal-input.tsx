77c9a4dbd7d209587ed12dae137785cc9c944503
Lennart Horn <lennart.horn@buchhaus.ch>
2026-01-02 22:56:29 +0100

    Minor changes

--- apps/web/components/custom/multimodal-input.tsx
+++ apps/web/components/custom/multimodal-input.tsx
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Modifications copyright (C) 2025 <NextGpt.ai Technologies>
+// Modifications copyright (C) 2025 <Lennart Horn>
 
 "use client";
 
--- packages/ui/src/hooks/use-scroll-to-bottom.ts
+++ packages/ui/src/hooks/use-scroll-to-bottom.ts
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Modifications copyright (C) 2025 <NextGpt.ai Technologies>
+// Modifications copyright (C) 2025 <Lennart Horn>
 
 import { useEffect, useRef, type RefObject } from "react";
 
